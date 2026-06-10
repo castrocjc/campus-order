85bf7af104571189475193c776bcd70c73265fcf
Juan Carlos Castro Cruz <jc.castroc@gmail.com>
2026-06-09 21:49:00 -0600

    feat: enable Gmail SMTP email sending

--- backend/src/main/java/com/campusorder/service/EmailService.java
+++ backend/src/main/java/com/campusorder/service/EmailService.java
@@ -1,11 +1,37 @@
 package com.campusorder.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     public void sendVerificationCode(String to, String code) {
-        System.out.println("Código de verificación para " + to + ": " + code);
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(fromEmail);
+        message.setTo(to);
+        message.setSubject("Verificación de correo - CofiGO");
+
+        message.setText(
+                "Hola,\n\n" +
+                "Tu código de verificación para CofiGO es:\n\n" +
+                code +
+                "\n\n" +
+                "Este código expira en 10 minutos.\n\n" +
+                "Equipo CofiGO"
+        );
+
+        mailSender.send(message);
     }
 }
